87453c5de5567c560fb348d77087aeff8421d645
mikel366 <mikelburns25@gmail.com>
2024-06-15 08:56:14 -0300

    agregado de modal de renovar password

--- plantilla/vistas/modulos/login.php
+++ plantilla/vistas/modulos/login.php
@@ -17,14 +17,14 @@
 
                         <form method="post">
 
-                        <div class="row">
-                            <div class="col-12">
-                                <?php
+                            <div class="row">
+                                <div class="col-12">
+                                    <?php
                                     $login = new UsuariosControlador();
                                     $login->ctrIngresoUsuario()
-                                ?>
+                                    ?>
+                                </div>
                             </div>
-                        </div>
 
                             <div class="form-group mb-3">
                                 <label class="form-label" for="emailaddress">Email address</label>
@@ -43,7 +43,7 @@
                                     <label class="form-check-label ms-2" for="checkbox-signin">Remember me</label>
                                 </div>
                             </div>
-                            
+
 
                             <div class="form-group mb-0 text-center">
                                 <button class="btn btn-primary w-100" type="submit"> Log In </button>
@@ -55,7 +55,7 @@
 
                 <div class="row mt-3">
                     <div class="col-12 text-center">
-                        <p class="text-white-50"> <a href="pages-register.html" class="text-white-50 ms-1">Forgot your password?</a></p>
+                        <p class="text-white-50"> <a href="javascript:void(0)" data-bs-toggle="modal" data-bs-target="#renovarModal" class="text-white-50 ms-1">Forgot your password?</a></p>
                         <p class="text-white-50">Don't have an account? <a href="pages-register.html" class="text-white font-weight-medium ms-1">Sign Up</a></p>
                     </div> <!-- end col -->
                 </div>
@@ -64,5 +64,34 @@
             </div> <!-- end col -->
         </div>
         <!-- end row -->
+    </div>
+
+    <div class="modal fade" id="renovarModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form method="post">
+                    <div class="modal-header bg-primary">
+                        <h5 class="modal-title"> Renovar contraseña</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body p-4">
+                        <div class="col-md-12">
+                            <div class="mb-3">
+                                <label for="form-label">Ingrese Email</label>
+                                <input type="text" class="form-control" id="nombre_marca" name="resetPassword" required>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                        <button type="submit" class="btn btn-info">Guardar</button>
+                    </div>
+                    <?php
+                    $renovar = new UsuariosControlador();
+                    $renovar->ctrRenovarPassword();
+                    ?>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
